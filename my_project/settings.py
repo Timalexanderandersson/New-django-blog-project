2b03e495c54f081024a5b09b298744534277968c
Tim Andersson <timalexanderandersson@hotmail.com>
2024-08-07 14:47:39 +0000

    for heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-96qr)j-9ec3g&u+i_@np=bf=09fng12kgu^_ax(no*71!o+$h2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-timalexande-newdjangopr-qmhk5hx8p5i.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-timalexande-newdjangopr-qmhk5hx8p5i.ws.codeinstitute-ide.net',
+,'.herokuapp.com']
 
 
 # Application definition
