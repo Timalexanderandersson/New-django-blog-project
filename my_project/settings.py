3850754272e9139e5898b94fe89bd95a47563498
Tim Andersson <timalexanderandersson@hotmail.com>
2024-08-07 15:10:50 +0000

    added

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-96qr)j-9ec3g&u+i_@np=bf=09fng12kgu^_ax(no*71!o+$h2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-timalexande-newdjangopr-qmhk5hx8p5i.ws.codeinstitute-ide.net',
-,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-timalexande-newdjangopr-qmhk5hx8p5i.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
